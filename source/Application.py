1d4b95e0062ab419f76da50d637351b912b3a2dc
pyonpyon7654 <mahjiaqi@hotmail.com>
2020-02-23 05:54:50 +0800

    Clean up codes

--- source/Application.py
+++ source/Application.py
@@ -5,9 +5,6 @@ import fiona  # ; help(fiona.open)
 from FrameGUI import *
 from MapNode import *
 
-import random  # TEMPORARY
-
-
 class Application():
 
     def __init__(self):
@@ -44,21 +41,6 @@ class Application():
         for i in range(0, len(lrt)):
             self.lrt_nodes.append(MapNode(lrt.name[i], "lrt", lrt.geometry.y[i], lrt.geometry.x[i]))
             print(lrt.name[i], lrt.geometry.x[i], lrt.geometry.y[i], "loaded")
-
-
-        # TEMPORARY
-        # REMOVE LATER ON
-        # self.lrt_nodes.append(MapNode("Soo Teck LRT", "lrt", 1.405392, 103.897285))
-        # self.lrt_nodes.append(MapNode("Sumang LRT", "lrt", 1.408428, 103.898619))
-        # self.lrt_nodes.append(MapNode("Nibong", "lrt", 1.411841, 103.900335))
-        #for i in range(50):
-        #     self.lrt_nodes.append(MapNode("test", "lrt", random.uniform(1.385900, 1.421450), random.uniform(103.886964, 103.931048)))
-        # for i in range(50):
-        #     self.mrt_nodes.append(MapNode("test", "mrt", random.uniform(1.385900, 1.421450), random.uniform(103.886964, 103.931048)))
-        # for i in range(50):
-        #      self.bus_stop_nodes.append(MapNode("test", "bus", random.uniform(1.385900, 1.421450), random.uniform(103.886964, 103.931048)))
-        # for i in range(500):
-        #     self.hdb_nodes.append(MapNode("test", "hdb", random.uniform(1.385900, 1.421450), random.uniform(103.886964, 103.931048)))
 
     def test_function(self):
         print("Hi")
