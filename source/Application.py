bbed0cbaf0144ff86f67edcee31d161e3fdaec82
pyonpyon7654 <mahjiaqi@hotmail.com>
2020-03-19 00:46:48 +0800

    delete algorithm

--- source/Application.py
+++ source/Application.py
@@ -69,69 +69,6 @@ class Application():
         print("Finding path from '" + str(self.selected_start_node.node_name) + "' to '" + str(self.selected_end_node.node_name) + "'");
         #self.dijkstra(self.selected_start_node, self.selected_end_node)
 
-
-    ## TODO: found on youtube, need modify, need find map link using dictonary https://www.youtube.com/watch?v=Ub4-nG09PFw
-    map = {
-        'a': {'b': 3, 'c': 4, 'd': 7},
-        'b': {'c': 1, 'f': 5},
-        'c': {'f': 6, 'd': 2},
-        'd': {'e': 3, 'g': 6},
-        'e': {'g': 3, 'h': 4},
-        'f': {'e': 1, 'h': 8},
-        'g': {'h': 2},
-        'h': {'g': 2}
-    }
-
-    def dijkstra(map, start, end):
-        shortest_distance = {}
-        track_predecessor = {}
-        unseenNodes = map
-        infinity = 99999
-        track_path = []  # dictionary to record as we trace back our journey
-
-        for node in unseenNodes:
-            shortest_distance[node] = infinity
-
-        shortest_distance[start] = 0
-
-        while unseenNodes:
-            min_distance_node = None
-
-            for node in unseenNodes:
-                if min_distance_node is None:
-                    min_distance_node = node
-
-                elif shortest_distance[node] < shortest_distance[min_distance_node]:
-                    min_distance_node = node
-
-            path_options = map[min_distance_node].items()
-
-            for child_node, weight in path_options:
-
-                if weight + shortest_distance[min_distance_node] < shortest_distance[child_node]:
-                    shortest_distance[child_node] = weight + shortest_distance[min_distance_node]
-
-                    track_predecessor[child_node] = min_distance_node
-
-            unseenNodes.pop(min_distance_node)
-        currentNode = end
-
-        while currentNode != start:
-            try:
-                track_path.insert(0, currentNode)
-                currentNode = track_predecessor[currentNode]
-            except KeyError:
-                print('Path not reachable')
-                break
-        track_path.insert(0, start)
-
-        if shortest_distance[end] != infinity:
-            print('Shortest distance is ' + str(shortest_distance[end]))
-            print('And the path is ' + str(track_path))
-
-    dijkstra(map, 'a', 'h')
-
-
 def get_lrt():
     lrtnames = []
     lrt = geopandas.read_file("map/new_lrt.geojson")
